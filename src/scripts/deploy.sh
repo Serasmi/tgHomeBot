b5a68faf8ed48107db8eff19eb2492ddf10c07fb
Serasmi <serasmi@yandex.ru>
2022-03-26 18:02:43 +0300

    chore: config deployment

---
+++ src/scripts/deploy.sh
@@ -0,0 +1,8 @@
+# 1. Copy content of dist folder to remote folder
+# 2. Copy files to remote folder
+#   - package.json
+#   - yarn.lock
+#   - Dockerfile
+#   - .dockerigonre
+#   - .env
+# 3. build and run docker image remotely
